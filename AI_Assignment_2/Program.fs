7322a6a413f5570a559d1dd7ad3b6718a5bb2f21
Allen Simpson <allen.simpson@motionencoding.com>
2018-11-15 22:59:12 -0700

    Commenting

--- AI_Assignment_2/Program.fs
+++ AI_Assignment_2/Program.fs
@@ -7,44 +7,23 @@ open Utility
 
 [<EntryPoint>]
 let main argv =
-    let runMazeState =
+    let runMazeState =  //runMazeState will be...
         match argv.[0].ToLowerInvariant() with
-        | "--simple"
-        | "-s" -> Utility.runMazeStateSimple
-        | "--forward"
-        | "-f" -> Utility.runMazeStateForward
-        | unknown -> failwithf "I don't know what %s means" unknown
-    let mazeDirectory= if argv.Length >1 then argv.[1] else "../mazes"
-    printfn "\n================================================================================"
+        | "--simple"                            //|
+        | "-s" -> Utility.runMazeStateSimple    //The Simple Maze   
+        | "--forward"                           //or
+        | "-f" -> Utility.runMazeStateForward   //The Forward Maze
+        | unknown -> failwithf "I don't know what %s means" unknown //you gave it a different argument
+    let mazeDirectory= if argv.Length >1 then argv.[1] else "../mazes"  //go fin the mazes
+    printfn "\n================================================================================"    //print beutification lines
     System.IO.Directory.EnumerateFiles(mazeDirectory, "*.txt")
-    |> Seq.map readMaze
-    |> Seq.map createMazeState
-    |> Seq.map (search runMazeState)
-    |> Seq.iter (function
-        | None -> printfn "No valid mazes"
-        | Some (solution,count) -> (*printfn "Solution found in (%d) steps" count;*) printMazeState solution; printfn "================================================================================")
-    printfn ("All Mazes finished!")
-
-    
-
-(*
-    //"../mazes/4x4maze.txt"
-    
-    argv.[1]
-    |> readMaze
-    |> createMazeState
-    |> search runMazeState
-    |> function
-        | None -> printfn("No solution")
-        | Some (solution,count) -> printfn "Solution found in (%d) steps" count; printMazeState solution
-                
-
-
-                *)
-
-
-
-
+    |> Seq.map readMaze //get the maze
+    |> Seq.map createMazeState  //create a mazeState
+    |> Seq.map (search runMazeState)    //run the algorithim of your choice on every maze in ../mazes
+    |> Seq.iter (function               //and look at each maze
+        | None -> printfn "No valid mazes"  //if there was no solution say so, else..
+        | Some (solution,count) -> printMazeState solution; printfn "================================================================================") //print beutification lines and the finished maze
+    printfn ("All Mazes finished!")     //print the finish comment!
 
 
     0 // return an integer exit code
